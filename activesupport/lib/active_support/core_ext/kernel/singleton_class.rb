4316f5aeccf9a4e3a2acc3125c120486a920a920
David.Gao <david.alpha.fox@gmail.com>
2017-10-05 22:33:47 +0800

    singleton_class overwrite

--- activesupport/lib/active_support/core_ext/kernel/singleton_class.rb
+++ activesupport/lib/active_support/core_ext/kernel/singleton_class.rb
@@ -2,9 +2,38 @@
 
 module Kernel
   # class_eval on an object acts like singleton_class.class_eval.
-  ## overwrite Kernel.class_eval ?
-  ## what's different between Kernel.class_eval and singleton_class.class_eval?
+  ## overwrite Kernel.class_eval
+  ## after doing this
+  ## we can let obejct have the ability of class_eval
   def class_eval(*args, &block)
     singleton_class.class_eval(*args, &block)
   end
 end
+=begin
+  after doing this
+  we can have this
+
+  class A
+    def show
+      puts "class A"
+    end
+  end
+  a = A.new
+  a.class_eval "def instance_show ; puts \"instance A\"; end;"
+
+  And it same as below before overwrite Kernel module
+  class A
+    def show
+      puts "class A"
+    end
+  end
+  a = A.new
+  class << a
+    def instance_show
+      puts "instance A"
+    end
+  end
+
+Object is a Class and Class is a Object
+Object is an instance Class but Class isn`t an instance Object
+=end
